05e1ed37de8d516915eb19676f7e1bce68ca68b4
Teoman Soygul <teo@soygul.com>
2014-05-30 23:09:28 +0200

    update a.studio

--- app/src/main/java/com/nbusy/app/DataService.java
+++ app/src/main/java/com/nbusy/app/DataService.java
@@ -2,9 +2,13 @@ package com.nbusy.app;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.IBinder;
+import android.util.LruCache;
 
 public class DataService extends Service {
+    private LruCache<String, Bitmap> profilesMemoryCache;
+
     public DataService() {
     }
 
