fab324892d8afad62935e4a6f92c6356195d7a18
Teoman Soygul <teo@soygul.com>
2014-06-08 10:42:12 +0200

    add post json

--- app/src/main/java/com/nbusy/app/data/JsonClient.java
+++ app/src/main/java/com/nbusy/app/data/JsonClient.java
@@ -3,8 +3,11 @@ package com.nbusy.app.data;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,11 +17,14 @@ import java.util.logging.Logger;
  * HTTP client with JSON content type by default.
  */
 public class JsonClient {
-    public <T> T getJSON(String uri, java.lang.Class<T> classOfT) {
+    private final Gson gson = new Gson();
+
+    public <T> T get(String uri, Class<T> dataType) {
         try {
             URL url = new URL(uri);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
             connection.connect();
             int status = connection.getResponseCode();
 
@@ -29,11 +35,48 @@ public class JsonClient {
                     StringBuilder response = new StringBuilder();
                     String line;
                     while ((line = reader.readLine()) != null) {
-                        response.append(line).append("\n");
+                        response.append(line);
+                    }
+                    reader.close();
+                    return gson.fromJson(response.toString(), dataType);
+            }
+
+        } catch (MalformedURLException ex) {
+            //Logger.getLogger(DebugServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            //Logger.getLogger(DebugServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public <T> T post(String uri, Object data, Class<T> dataType) {
+        try {
+            URL url = new URL(uri);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+            String postData = new Gson().toJson(data);
+            connection.connect();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+            writer.write(postData);
+            writer.flush();
+            writer.close();
+            int status = connection.getResponseCode();
+
+            switch (status) {
+                case 200:
+                case 201:
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
                     }
                     reader.close();
                     Gson gson = new Gson();
-                    return gson.fromJson(response.toString(), classOfT);
+                    return gson.fromJson(response.toString(), dataType);
             }
 
         } catch (MalformedURLException ex) {
