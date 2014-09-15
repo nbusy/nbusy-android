7fa5c8e9786f3daee48c891856d2999eee68c262
Teoman Soygul <teo@soygul.com>
2014-09-15 12:01:44 +0200

    tidy up msg detail act.

--- app/src/main/java/com/nbusy/app/MessageListActivity.java
+++ app/src/main/java/com/nbusy/app/MessageListActivity.java
@@ -1,34 +1,23 @@
 package com.nbusy.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-
-
-
 
 /**
- * An activity representing a list of Messages. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link MessageDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- * <p>
- * The activity makes heavy use of fragments. The list of items is a
- * {@link MessageListFragment} and the item details
+ * An activity representing a list of Messages. This activity has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched, lead to a {@link MessageDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and item details side-by-side using two vertical panes.
+ * <p/>
+ * The activity makes heavy use of fragments. The list of items is a {@link MessageListFragment} and the item details
  * (if present) is a {@link MessageDetailFragment}.
- * <p>
- * This activity also implements the required
- * {@link MessageListFragment.Callbacks} interface
- * to listen for item selections.
+ * <p/>
+ * This activity also implements the required {@link MessageListFragment.Callbacks} interface to listen for item selections.
  */
-public class MessageListActivity extends Activity
-        implements MessageListFragment.Callbacks {
+public class MessageListActivity extends Activity implements MessageListFragment.Callbacks {
 
     /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet device.
      */
     private boolean mTwoPane;
 
@@ -38,14 +27,11 @@ public class MessageListActivity extends Activity
         setContentView(R.layout.activity_message_list);
 
         if (findViewById(R.id.message_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-large and
-            // res/values-sw600dp). If this view is present, then the
-            // activity should be in two-pane mode.
+            // The detail container view will be present only in the large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the activity should be in two-pane mode.
             mTwoPane = true;
 
-            // In two-pane mode, list items should be given the
-            // 'activated' state when touched.
+            // In two-pane mode, list items should be given the 'activated' state when touched.
             ((MessageListFragment) getFragmentManager()
                     .findFragmentById(R.id.message_list))
                     .setActivateOnItemClick(true);
@@ -55,15 +41,12 @@ public class MessageListActivity extends Activity
     }
 
     /**
-     * Callback method from {@link MessageListFragment.Callbacks}
-     * indicating that the item with the given ID was selected.
+     * Callback method from {@link MessageListFragment.Callbacks} indicating that the item with the given ID was selected.
      */
     @Override
     public void onItemSelected(String id) {
         if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
+            // In two-pane mode, show the detail view in this activity by adding or replacing the detail fragment using a fragment transaction.
             Bundle arguments = new Bundle();
             arguments.putString(MessageDetailFragment.ARG_ITEM_ID, id);
             MessageDetailFragment fragment = new MessageDetailFragment();
@@ -73,8 +56,7 @@ public class MessageListActivity extends Activity
                     .commit();
 
         } else {
-            // In single-pane mode, simply start the detail activity
-            // for the selected item ID.
+            // In single-pane mode, simply start the detail activity for the selected item ID.
             Intent detailIntent = new Intent(this, MessageDetailActivity.class);
             detailIntent.putExtra(MessageDetailFragment.ARG_ITEM_ID, id);
             startActivity(detailIntent);
