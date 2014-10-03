fb6daab3bb3f2efa54ff8a41968a72e437bafd4d
Teoman Soygul <teo@soygul.com>
2014-10-03 11:46:09 +0200

    updated intellij & gradle

--- app/src/main/java/com/nbusy/app/GcmIntentService.java
+++ app/src/main/java/com/nbusy/app/GcmIntentService.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 //package com.nbusy.app;
 //
 //import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -29,12 +13,12 @@
 //import android.util.Log;
 //
 ///**
-// * This {@code IntentService} does the actual handling of the GCM message.
-// * {@code GcmBroadcastReceiver} (a {@code WakefulBroadcastReceiver}) holds a
-// * partial wake lock for this service while the service does its work. When the
-// * service is finished, it calls {@code completeWakefulIntent()} to release the
-// * wake lock.
-// */
+//* This {@code IntentService} does the actual handling of the GCM message.
+//* {@code GcmBroadcastReceiver} (a {@code WakefulBroadcastReceiver}) holds a
+//* partial wake lock for this service while the service does its work. When the
+//* service is finished, it calls {@code completeWakefulIntent()} to release the
+//* wake lock.
+//*/
 //public class GcmIntentService extends IntentService {
 //    public static final int NOTIFICATION_ID = 1;
 //    private NotificationManager mNotificationManager;
