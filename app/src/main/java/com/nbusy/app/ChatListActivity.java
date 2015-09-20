1ff8af0e0abf1eeeb983f1ebc548b0ed3a53e85d
Teoman Soygul <tsoygul@gmail.com>
2015-09-20 16:46:07 +0200

    backport gcm handler code

--- app/src/main/java/com/nbusy/app/ChatListActivity.java
+++ app/src/main/java/com/nbusy/app/ChatListActivity.java
@@ -1,8 +1,19 @@
 package com.nbusy.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -23,6 +34,14 @@ import android.app.Activity;
  */
 public class ChatListActivity extends Activity
         implements ChatListFragment.Callbacks {
+
+    private static final String TAG = "MessageListActivity";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private static final String PROPERTY_REG_ID = "registration_id";
+    GoogleCloudMessaging gcm;
+    String SENDER_ID = "218602439235";
+    String regId;
+    AtomicInteger msgId = new AtomicInteger();
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
@@ -50,6 +69,17 @@ public class ChatListActivity extends Activity
         }
 
         // TODO: If exposing deep links into your app, handle intents here.
+
+        // GCM registration
+        gcm = GoogleCloudMessaging.getInstance(this);
+        regId = getRegistrationId(getApplicationContext());
+        if (regId.isEmpty()) {
+            registerInBackground();
+        }
+
+        sendGcmMessage("just testing from Android simulator");
+        sendGcmMessage("test 2");
+        sendGcmMessage("test 3");
     }
 
     /**
@@ -77,5 +107,137 @@ public class ChatListActivity extends Activity
             detailIntent.putExtra(ChatDetailFragment.ARG_ITEM_ID, id);
             startActivity(detailIntent);
         }
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * Gets the current registration ID for application on GCM service, if there is one. If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing registration ID.
+     */
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGcmPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "GCM registration not found.");
+            return "";
+        }
+
+        // check if app was updated; if so, it must clear the registration ID since the existing regID is not guaranteed to work with
+        // the new app version
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed. GCM registration will be refreshed.");
+            return "";
+        }
+
+        Log.i(TAG, "GCM registration id found: " + registrationId);
+        return registrationId;
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * Stores the registration ID and the app versionCode in the application's shared preferences.
+     */
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+                    }
+                    regId = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regId;
+
+                    // you should send the registration ID to your server over HTTP, so it can use GCM/HTTP or CCS to send messages to your app
+                    sendRegistrationIdToBackend();
+
+                    // For this demo: we don't need to send it because the device will send upstream messages to a server that echo back the message
+                    // using the 'from' address in the message.
+
+                    // persist the regID - no need to register again
+                    storeRegistrationId(getApplicationContext(), regId);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // if there is an error, don't just keep trying to register
+                    // require the user to click a button again, or perform exponential back-off
+                }
+
+                Log.i(TAG, msg);
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                Log.i(TAG, msg);
+            }
+        }.execute(null, null, null);
+    }
+
+    /**
+     * Stores the registration ID and the app versionCode in the application's {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId   registration ID
+     */
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGcmPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.apply();
+    }
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private SharedPreferences getGcmPreferences(Context context) {
+        // this sample app persists the registration ID in shared preferences, but how you store the regID in your app is up to you
+        return getSharedPreferences(ChatListActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+    }
+
+    private void sendRegistrationIdToBackend() {
+        sendGcmMessage(regId);
+    }
+
+    private void sendGcmMessage(final String message) {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    Bundle data = new Bundle();
+                    data.putString("test_message_from_device", message);
+                    String id = Integer.toString(msgId.incrementAndGet());
+                    gcm.send(SENDER_ID + "@gcm.googleapis.com", id, data);
+                    msg = "Sent upstream GCM message to the backend: " + message;
+                } catch (IOException ex) {
+                    msg = "Error while sending upstream GCM message to the backend:" + ex.getMessage();
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                Log.i(TAG, msg);
+            }
+        }.execute(null, null, null);
     }
 }
