a9ae462ee1f57915be2eecc9394a21cf347b7581
Teoman Soygul <tsoygul@gmail.com>
2015-10-16 01:33:54 +0200

    don't use member prefixes

--- app/src/main/java/com/nbusy/app/ChatListFragment.java
+++ app/src/main/java/com/nbusy/app/ChatListFragment.java
@@ -12,7 +12,7 @@ import android.widget.ListView;
  * also supports tablet devices by allowing list items to be given an
  * 'activated' state upon selection. This helps indicate which item is
  * currently being viewed in a {@link ChatDetailFragment}.
- * <p/>
+ * <p>
  * Activities containing this fragment MUST implement the {@link Callbacks}
  * interface.
  */
@@ -27,17 +27,16 @@ public class ChatListFragment extends ListFragment {
     /**
      * The fragment's current callback object, which is notified of list item clicks.
      */
-    private Callbacks mCallbacks = sDummyCallbacks;
+    private Callbacks callbacks;
 
     /**
      * The current activated item position. Only used on tablets.
      */
-    private int mActivatedPosition = ListView.INVALID_POSITION;
+    private int activatedPosition = ListView.INVALID_POSITION;
 
     /**
-     * A callback interface that all activities containing this fragment must
-     * implement. This mechanism allows activities to be notified of item
-     * selections.
+     * A callback interface that all activities containing this fragment must implement.
+     * This mechanism allows activities to be notified of item selections.
      */
     public interface Callbacks {
         /**
@@ -45,16 +44,6 @@ public class ChatListFragment extends ListFragment {
          */
         void onItemSelected(String id);
     }
-
-    /**
-     * A dummy implementation of the {@link Callbacks} interface that does
-     * nothing. Used only when this fragment is not attached to an activity.
-     */
-    private static Callbacks sDummyCallbacks = new Callbacks() {
-        @Override
-        public void onItemSelected(String id) {
-        }
-    };
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -66,19 +55,15 @@ public class ChatListFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setListAdapter(new ChatListArrayAdapter(
-                getActivity(),
-                Chats.ITEMS));
+        setListAdapter(new ChatListArrayAdapter(getActivity(), Chats.ITEMS));
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Restore the previously serialized activated item position.
-        if (savedInstanceState != null
-                && savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
+        // restore the previously serialized 'activated item' position
+        if (savedInstanceState != null && savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
             setActivatedPosition(savedInstanceState.getInt(STATE_ACTIVATED_POSITION));
         }
     }
@@ -87,26 +72,22 @@ public class ChatListFragment extends ListFragment {
     public void onAttach(Context context) {
         super.onAttach(context);
 
-        if (!(context instanceof Activity)){
-            throw new IllegalStateException("ChatListFragment should be attached to an Activity.");
-        }
+        Activity activity = context instanceof Activity ? (Activity) context : null;
 
-        Activity activity = (Activity) context;
-
-        // Activities containing this fragment must implement its callbacks.
+        // activities containing this fragment must implement its callbacks
         if (!(activity instanceof Callbacks)) {
             throw new IllegalStateException("Host Activity must implement ChatListFragment's callbacks.");
         }
 
-        mCallbacks = (Callbacks) activity;
+        callbacks = (Callbacks) activity;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
 
-        // Reset the active callbacks interface to the dummy implementation.
-        mCallbacks = sDummyCallbacks;
+        // reset the active callbacks interface to the dummy implementation on detach
+        callbacks = null;
     }
 
     @Override
@@ -115,15 +96,17 @@ public class ChatListFragment extends ListFragment {
 
         // Notify the active callbacks interface (the activity, if the
         // fragment is attached to one) that an item has been selected.
-        mCallbacks.onItemSelected(Chats.ITEMS.get(position).name);
+        if (callbacks != null) {
+            callbacks.onItemSelected(Chats.ITEMS.get(position).name);
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (mActivatedPosition != ListView.INVALID_POSITION) {
+        if (activatedPosition != ListView.INVALID_POSITION) {
             // Serialize and persist the activated item position.
-            outState.putInt(STATE_ACTIVATED_POSITION, mActivatedPosition);
+            outState.putInt(STATE_ACTIVATED_POSITION, activatedPosition);
         }
     }
 
@@ -141,11 +124,11 @@ public class ChatListFragment extends ListFragment {
 
     private void setActivatedPosition(int position) {
         if (position == ListView.INVALID_POSITION) {
-            getListView().setItemChecked(mActivatedPosition, false);
+            getListView().setItemChecked(activatedPosition, false);
         } else {
             getListView().setItemChecked(position, true);
         }
 
-        mActivatedPosition = position;
+        activatedPosition = position;
     }
 }
