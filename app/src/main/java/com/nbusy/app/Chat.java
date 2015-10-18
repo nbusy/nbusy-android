f6c6458f18d6ae6c5f529de0fc4b1af5c0b4e688
Teoman Soygul <tsoygul@gmail.com>
2015-10-18 16:42:48 +0200

    refactor Chat class

--- app/src/main/java/com/nbusy/app/Chat.java
+++ app/src/main/java/com/nbusy/app/Chat.java
@@ -1,12 +1,15 @@
 package com.nbusy.app;
 
+/**
+ * One-to-one chat.
+ */
 final class Chat {
-    public final String name; // peer name
+    public final String peerName; // peer name
     public final String lastMessage; // last message in conversation
     public final String sent; // last message sent date/time
 
-    public Chat(String name, String lastMessage, String sent) {
-        this.name = name;
+    public Chat(String peerName, String lastMessage, String sent) {
+        this.peerName = peerName;
         this.lastMessage = lastMessage;
         this.sent = sent;
     }
