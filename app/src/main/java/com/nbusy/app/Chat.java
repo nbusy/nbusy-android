ddcc93c421522059442448739b9dce9d4b439937
Teoman Soygul <tsoygul@gmail.com>
2015-10-18 21:59:50 +0200

    Chat now contains the message list

--- app/src/main/java/com/nbusy/app/Chat.java
+++ app/src/main/java/com/nbusy/app/Chat.java
@@ -1,16 +1,24 @@
 package com.nbusy.app;
 
+import com.google.common.collect.ImmutableList;
+
 /**
  * One-to-one chat.
  */
 final class Chat {
-    public final String peerName; // peer name
-    public final String lastMessage; // last message in conversation
-    public final String sent; // last message sent date/time
+    final String peerName; // peer name
+    final String lastMessage; // last message in conversation
+    final String sent; // last message sent date/time
+    final ImmutableList<Message> messages; // list of messages in this chat
 
-    public Chat(String peerName, String lastMessage, String sent) {
+    Chat(String peerName, String lastMessage, String sent) {
+        this(peerName, lastMessage, sent, ImmutableList.<Message>of());
+    }
+
+    Chat(String peerName, String lastMessage, String sent, ImmutableList<Message> messages) {
         this.peerName = peerName;
         this.lastMessage = lastMessage;
         this.sent = sent;
+        this.messages = messages;
     }
 }
--- app/src/main/java/com/nbusy/app/Message.java
+++ app/src/main/java/com/nbusy/app/Message.java
@@ -4,9 +4,9 @@ package com.nbusy.app;
  * A message within a chat.
  */
 final class Message {
-    public final String from; // sender of this message
-    public final String body; // message text
-    public final String sent; // message sent date/time
+    final String from; // sender of this message
+    final String body; // message text
+    final String sent; // message sent date/time
 
     Message(String from, String body, String sent) {
         this.from = from;
