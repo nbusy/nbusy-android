2f10f3ee9d6d5d31ca387d57dc1cabe6595e0422
Teoman Soygul <tsoygul@gmail.com>
2015-10-27 18:56:48 +0100

    clear worker service details for now

--- app/src/main/java/com/nbusy/app/WorkerService.java
+++ app/src/main/java/com/nbusy/app/WorkerService.java
@@ -7,15 +7,6 @@ import android.os.IBinder;
 public class WorkerService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
-        // don't allow direct binding to this service
         return null;
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // todo: initiate connection to nbusy server using neptulon json-rpc java client (or nbusy java client which wraps that and auto-adds all routes)?
-
-        // we want this service to continue running until it is explicitly stopped, so return sticky
-        return START_STICKY;
     }
 }
