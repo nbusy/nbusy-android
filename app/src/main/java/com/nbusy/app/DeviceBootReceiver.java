0c62689024fc81b0b243b171df0d4550425e6728
Teoman Soygul <tsoygul@gmail.com>
2015-10-27 14:16:59 +0100

    add DeviceBootReceiver

---
+++ app/src/main/java/com/nbusy/app/DeviceBootReceiver.java
@@ -0,0 +1,12 @@
+package com.nbusy.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DeviceBootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
