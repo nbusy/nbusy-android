bc94f5f9ad1352696415807055562d356b8176b5
Teoman Soygul <tsoygul@gmail.com>
2015-10-28 14:38:28 +0100

    fix reverted comparison order

--- app/src/main/java/com/nbusy/app/DeviceBootBroadcastReceiver.java
+++ app/src/main/java/com/nbusy/app/DeviceBootBroadcastReceiver.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 public class DeviceBootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             Intent i = new Intent(context, WorkerService.class);
             i.putExtra(WorkerService.STARTED_BY, DeviceBootBroadcastReceiver.class.getSimpleName());
             context.startService(i);
