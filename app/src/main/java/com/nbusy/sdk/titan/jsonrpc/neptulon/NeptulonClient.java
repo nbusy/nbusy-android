6f238e0bfb677ed14a2a3306179542be1374f7e5
Teoman Soygul <tsoygul@gmail.com>
2015-11-10 09:50:58 +0100

    separate socket factory creation

--- app/src/main/java/com/nbusy/sdk/titan/jsonrpc/neptulon/NeptulonClient.java
+++ app/src/main/java/com/nbusy/sdk/titan/jsonrpc/neptulon/NeptulonClient.java
@@ -2,13 +2,10 @@ package com.nbusy.sdk.titan.jsonrpc.neptulon;
 
 import android.net.SSLCertificateSocketFactory;
 
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 
 import javax.net.ssl.SSLSocket;
@@ -18,17 +15,26 @@ import javax.net.ssl.TrustManagerFactory;
  * Neptulon client implementation: https://github.com/neptulon/neptulon
  */
 public class NeptulonClient implements Neptulon {
-    private final SSLSocket socket;
+    private SSLSocket socket;
 
-    public NeptulonClient(InputStream caCert) throws IOException, CertificateException, KeyStoreException, NoSuchAlgorithmException {
-        // todo: provide session cache instance and limit ciphers
+    public void connect(String pemEncodedCaCert) {
+        InputStream caCertStream = new ByteArrayInputStream(pemEncodedCaCert.getBytes());
+        SSLCertificateSocketFactory factory = getSocketFactory(caCertStream);
+
+        socket = (SSLSocket)factory.createSocket("localhost", 8081);
+    }
+
+    private SSLCertificateSocketFactory getSocketFactory(InputStream caCertStream) {
         SSLCertificateSocketFactory factory = (SSLCertificateSocketFactory)SSLCertificateSocketFactory.getDefault(60 * 1000, null);
-
-        // trust given CA certificate
         CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        Certificate ca = cf.generateCertificate(caCert);
-        String keyStoreType = KeyStore.getDefaultType();
-        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+
+        try {
+            Certificate ca = cf.generateCertificate(caCertStream);
+        } finally {
+            caCertStream.close();
+        }
+
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
         keyStore.load(null, null);
         keyStore.setCertificateEntry("ca", ca);
         String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
@@ -36,7 +42,6 @@ public class NeptulonClient implements Neptulon {
         tmf.init(keyStore);
         factory.setTrustManagers(tmf.getTrustManagers());
 
-        // create socket but don't connect yet
-        socket = (SSLSocket)factory.createSocket("localhost", 8081);
+        return factory;
     }
 }
