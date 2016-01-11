a93bb78ab622834d6498435239828a6b98371428
Teoman Soygul <tsoygul@gmail.com>
2016-01-11 23:43:34 +0100

    add unit test template

---
+++ app/src/test/java/NeptulonTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class NeptulonTest {
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertThat("hazelnuts", 3, equalTo(3));
+    }
+}
