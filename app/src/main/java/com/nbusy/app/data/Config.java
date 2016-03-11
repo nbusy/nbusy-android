d1699e6a859596c779dba1e3231cd790c4386598
Teoman Soygul <tsoygul@gmail.com>
2016-03-11 11:18:43 +0100

    list all possible servers in config

--- app/src/main/java/com/nbusy/app/data/Config.java
+++ app/src/main/java/com/nbusy/app/data/Config.java
@@ -3,16 +3,26 @@ package com.nbusy.app.data;
 import com.nbusy.app.BuildConfig;
 
 public class Config {
+    public static final String[] SERVERS = new String[] {"nbusy.com:80", "nbusy.com:3000"};
+
     public final Env env;
     public final String serverUrl;
 
+    /**
+     * Server URL should start with ws:// or wss://
+     */
     public Config(Env env, String serverUrl) {
         this.env = env;
         this.serverUrl = serverUrl;
     }
 
     public Config() {
-        this(BuildConfig.DEBUG ? Env.DEBUG : Env.PRODUCTION, "wss://nbusy.com");
+        env = BuildConfig.DEBUG ? Env.DEBUG : Env.PRODUCTION;
+        if (env == Env.PRODUCTION) {
+            serverUrl = "wss://" + SERVERS[0];
+        } else {
+            serverUrl = "ws://" + SERVERS[0];
+        }
     }
 
     public enum Env {
