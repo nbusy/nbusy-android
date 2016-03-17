bcc36bc7fb19eb1b2234219d2cc88239ed637fb8
Teoman Soygul <tsoygul@gmail.com>
2016-03-17 11:40:11 +0100

    chatlist: rename event handler to userProfileRetrievedEventHandler

--- app/src/main/java/com/nbusy/app/activities/ChatListFragment.java
+++ app/src/main/java/com/nbusy/app/activities/ChatListFragment.java
@@ -168,7 +168,7 @@ public class ChatListFragment extends ListFragment {
      ******************************/
 
     @Subscribe
-    public void userProfileReady(Worker.UserProfileRetrievedEvent e) {
+    public void userProfileRetrievedEventHandler(Worker.UserProfileRetrievedEvent e) {
         setListAdapter(new ChatListArrayAdapter(getActivity(), worker.userProfile.chats));
     }
 }
