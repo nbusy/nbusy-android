7ccaeaba8ba2813bef7a77a99cfbb71e73581fb6
Teoman Soygul <tsoygul@gmail.com>
2016-03-22 13:38:45 +0100

    WorkerService: move StopStandby into a subclass

--- app/src/main/java/com/nbusy/app/services/WorkerService.java
+++ app/src/main/java/com/nbusy/app/services/WorkerService.java
@@ -18,31 +18,29 @@ public class WorkerService extends Service {
     private static final String TAG = WorkerService.class.getSimpleName();
     public static final String STARTED_BY = "StartedBy";
     private static final int STANDBY_TIME = 3 * 60 * 1000;
+    private final StopStandby stopStandby = new StopStandby();
     private final Worker worker = WorkerSingleton.getWorker();
     private int startId;
 
-    private void init() {
-        class StopStandby extends AsyncTask<Void, Void, Void> {
-            @Override
-            protected Void doInBackground(Void... params) {
-                while (worker.needConnection()) {
-                    try {
-                        Thread.sleep(STANDBY_TIME);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+    class StopStandby extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            while (worker.needConnection()) {
+                try {
+                    Thread.sleep(STANDBY_TIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                return null;
             }
+            return null;
+        }
 
-            @Override
-            protected void onPostExecute(Void result) {
-                if (!stopSelfResult(startId)) {
-                    Log.e(TAG, "failed to stop service with startId: " + startId + " which did not match the one from the last start request");
-                }
+        @Override
+        protected void onPostExecute(Void result) {
+            if (!stopSelfResult(startId)) {
+                Log.e(TAG, "failed to stop service with startId: " + startId + " which did not match the one from the last start request");
             }
         }
-        new StopStandby().execute();
     }
 
     /*********************
@@ -66,7 +64,9 @@ public class WorkerService extends Service {
             Log.i(TAG, "Restarted by Android system after termination.");
         }
 
-        init();
+        if (stopStandby.getStatus() != AsyncTask.Status.RUNNING) {
+            stopStandby.execute();
+        }
 
         // we want this service to continue running until it is explicitly stopped, so return sticky
         return Service.START_STICKY;
