d330918cf518b6dfa82ece2b1eeb677ae8bd138d
Teoman Soygul <tsoygul@gmail.com>
2016-03-24 14:50:57 +0100

    Neptulon: add ReconnImpl

---
+++ app/src/main/java/neptulon/client/ReconnImpl.java
@@ -0,0 +1,53 @@
+package neptulon.client;
+
+import java.util.logging.Logger;
+
+import neptulon.client.callbacks.ConnCallback;
+
+/**
+ * This class extends {ConnImpl} with auto-reconnect functionality.
+ */
+public class ReconnImpl extends ConnImpl {
+    private static final Logger logger = Logger.getLogger("Titan: " + ReconnImpl.class.getSimpleName());
+    private boolean firstConnection = true;
+    private boolean manuallyDisconnected = false;
+    private boolean retriesExhausted = false;
+    private final int retryLimit = 5;
+    private int retryCount = 0;
+
+    @Override
+    public synchronized void connect(final ConnCallback cb) {
+        super.connect(new ConnCallback() {
+            @Override
+            public void connected() {
+                // only fire connected event once
+                if (firstConnection) {
+                    cb.connected();
+                }
+                firstConnection = false;
+            }
+
+            @Override
+            public void disconnected(String reason) {
+                // only fire disconnected event when retries are exhausted or on manual disconnect call
+                if (manuallyDisconnected || retriesExhausted) {
+                    cb.disconnected(reason);
+                    return;
+                }
+
+                // try to reconnect
+                if (retryCount <= retryLimit) {
+                    connect(this);
+                }
+
+                retryCount++;
+            }
+        });
+    }
+
+    @Override
+    public synchronized void close() {
+        manuallyDisconnected = true;
+        super.close();
+    }
+}
