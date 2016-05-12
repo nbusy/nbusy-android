f21f43512a9c4f225b5df889e580808d79093a3a
Teoman Soygul <tsoygul@gmail.com>
2016-05-12 10:34:53 +0200

    request profile info along with id token

--- app/src/main/java/com/nbusy/app/activities/LoginActivity.java
+++ app/src/main/java/com/nbusy/app/activities/LoginActivity.java
@@ -12,8 +12,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.nbusy.app.R;
 import com.nbusy.app.worker.Worker;
 import com.nbusy.app.worker.WorkerSingleton;
@@ -36,13 +34,12 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         // Button click listeners
         findViewById(R.id.login_button).setOnClickListener(this);
 
-        // Request only the user's ID token, which can be used to identify the
-        // user securely to your backend. This will contain the user's basic
-        // profile (name, profile picture URL, etc) so you should not need to
-        // make an additional call to personalize your application.
+        // Request only the user's ID token, which can be used to identify the user securely to your backend. This will contain the user's basic
+        // profile (name, profile picture URL, etc) so you should not need to make an additional call to personalize your application.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken("218602439235-6g09g0ap6i8v25v3rel49rtqjcu9ppj0.apps.googleusercontent.com")
                 .requestEmail()
+                .requestProfile()
                 .build();
 
         // Build GoogleAPIClient with the Google Sign-In API and the above options.
@@ -50,35 +47,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-    }
-
-    private void getIdToken() {
-        // Show an account picker to let the user choose a Google account from the device.
-        // If the GoogleSignInOptions only asks for IDToken and/or profile and/or email then no
-        // consent screen will be shown here.
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(signInIntent, RC_GET_TOKEN);
-        Log.d(TAG, "getIDToken: starting to get id token");
-    }
-
-    private void signOut() {
-        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        Log.d(TAG, "signOut:onResult:" + status);
-                    }
-                });
-    }
-
-    private void revokeAccess() {
-        Auth.GoogleSignInApi.revokeAccess(googleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        Log.d(TAG, "revokeAccess:onResult:" + status);
-                    }
-                });
     }
 
     @Override
@@ -115,5 +83,13 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 getIdToken();
                 break;
         }
+    }
+
+    private void getIdToken() {
+        // Show an account picker to let the user choose a Google account from the device.
+        // If the GoogleSignInOptions only asks for IDToken and/or profile and/or email then no consent screen will be shown here.
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_GET_TOKEN);
+        Log.d(TAG, "getIDToken: starting to get id token");
     }
 }
