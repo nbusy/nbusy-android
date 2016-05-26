b9053a9d05b3ad5ce82650d22f526d380f1db4b3
Teoman Soygul <tsoygul@gmail.com>
2016-05-26 12:14:28 +0200

    eventbus: move uithreadexecutor to correct package

--- app/src/main/java/com/nbusy/app/worker/eventbus/EventBus.java
+++ app/src/main/java/com/nbusy/app/worker/eventbus/EventBus.java
@@ -3,6 +3,10 @@ package com.nbusy.app.worker.eventbus;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * Event bus for passing information between activities and other components.
+ * Activities need an event bus and can't depend on regular callbacks due to their life-cycle.
+ */
 public class EventBus extends com.google.common.eventbus.EventBus {
     private final List<Object> subscribers = new CopyOnWriteArrayList<>();
 
