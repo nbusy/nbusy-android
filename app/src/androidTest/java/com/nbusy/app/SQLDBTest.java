dc430cf02bbb6a2b28fbc64221c63dbcbc75280d
Teoman Soygul <tsoygul@gmail.com>
2016-06-21 15:47:15 +0200

    test: add sqldb test case

---
+++ app/src/androidTest/java/com/nbusy/app/SQLDBTest.java
@@ -0,0 +1,18 @@
+package com.nbusy.app;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.nbusy.app.data.DB;
+import com.nbusy.app.data.sqldb.SQLDB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SQLDBTest {
+    @Test
+    public void getDB() throws Exception {
+        DB db = new SQLDB(InstrumentationRegistry.getContext());
+    }
+}
