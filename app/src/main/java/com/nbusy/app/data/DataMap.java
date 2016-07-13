dc4fa3049966906e5773dc9b92ee6f9ed977331b
Teoman Soygul <tsoygul@gmail.com>
2016-07-13 19:56:19 +0200

    add DataMap doc

--- app/src/main/java/com/nbusy/app/data/DataMap.java
+++ app/src/main/java/com/nbusy/app/data/DataMap.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 import titan.client.messages.MsgMessage;
 
+/**
+ * Converts objects used at different components to each other.
+ * i.e. A message retrieved from NBusy server into a message format to be displayed in an activity.
+ *
+ * Terminology:
+ * - Titan: Objects used by the Titan Java client.
+ * - NBusy: Objects used in (NBusy) app's /worker and /activities namespaces.
+ * - DB: Objects stored in app's database, which differ slightly from those used in activities for storage optimization.
+ */
 public class DataMap {
 
     public static MsgMessage[] nbusyToTitanMessages(com.nbusy.app.data.Message... msgs) {
